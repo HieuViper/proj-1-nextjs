173f35a7a373b799e54da47a1171002b8aa89af4
nguyenqghuy <nguyenqghuy@gmail.com>
2023-10-25 11:17:36 +0700

    correct models file name

--- src/app/(dashboard)/admin/page.js
+++ src/app/(dashboard)/admin/page.js
@@ -5,8 +5,13 @@ import { QueryTypes } from "sequelize";
 const DashboardPage = async () => {
   if( !db.initialized) {
     await db.initialize();
-  }
 
+  }
+  const createSampleData = () => {
+    // for ( let i = 1; i++; i < 100 ){
+    //   let news = db.News
+    // }
+  }
   return (
     <div>
       DashboardPage
